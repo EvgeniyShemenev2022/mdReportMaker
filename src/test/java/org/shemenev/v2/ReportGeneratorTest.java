3f540321f5791f1536e31a3cc6b8c0a1b21bf1ea
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2022-10-11 13:43:59 +0300

    Создан тест  ReportGeneratorTest

---
+++ src/test/java/org/shemenev/v2/ReportGeneratorTest.java
@@ -0,0 +1,38 @@
+package org.shemenev.v2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.NetworkInterface;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static org.junit.Assert.*;
+
+public class ReportGeneratorTest {
+
+    @Test
+    public void should_generate_report() {
+        List<Record> records = List.of(
+                new Record(1, LocalTime.of(10, 0, 0), "inbox", "начало"),
+                new Record(2, LocalTime.of(10, 30, 0), "break", "empty"),
+                new Record(3, LocalTime.of(11, 10, 0), "ID-1872", "делаю задачу"),
+                new Record(4, LocalTime.of(12, 0, 0), "end", "empty")
+        );
+        Map<String, Duration> actual = new ReportGenerator().generate(records);
+
+        Map<String, Duration> expectedMap = Map.ofEntries(
+                Map.entry("inbox", Duration.of(30, MINUTES)),
+                Map.entry("break", Duration.of(40, MINUTES)),
+                Map.entry("ID-1872", Duration.of(50, MINUTES))
+        );
+
+        Assert.assertTrue(actual.keySet().equals(expectedMap.keySet()));
+        Assert.assertTrue(actual.values().equals(expectedMap.values()));
+    }
+}
