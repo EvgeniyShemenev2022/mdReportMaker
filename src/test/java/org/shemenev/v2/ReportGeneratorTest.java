adec883f2ea6227c7056caf803dfdac2fe643161
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2022-10-14 12:09:34 +0300

    добавлены тесты

--- src/test/java/org/shemenev/v2/ReportGeneratorTest.java
+++ src/test/java/org/shemenev/v2/ReportGeneratorTest.java
@@ -34,7 +34,59 @@ public class ReportGeneratorTest {
                 Map.entry("ID-1872", Duration.of(50, MINUTES))
         );
 
-        Assert.assertTrue(actual.keySet().equals(expectedMap.keySet()));
-        Assert.assertTrue(actual.values().equals(expectedMap.values()));
+        assertEquals(actual.keySet(), expectedMap.keySet());
+        var actualList = new ArrayList<>(actual.values());
+        var expectedList = new ArrayList<>(expectedMap.values());
+        actualList.sort(null);
+        expectedList.sort(null);
+
+        assertEquals(actualList, expectedList);
+        // assertEquals(actual.values(), expectedMap.values());
+        assertEquals(actual.entrySet(), expectedMap.entrySet());
+    }
+
+    @Test
+    public void should_collect_report_map() {
+        List<Record> records = List.of(
+                new Record(1, LocalTime.of(10, 0, 0), "inbox", "начало"),
+                new Record(2, LocalTime.of(10, 30, 0), "break", "empty"),
+                new Record(3, LocalTime.of(11, 10, 0), "ID-1872", "делаю задачу"),
+                new Record(4, LocalTime.of(12, 0, 0), "inbox", "porn is good"),
+                new Record(5, LocalTime.of(14, 0, 0), "end", "empty")
+        );
+        Map<String, Duration> actual = new ReportGenerator().generate(records);
+
+        Map<String, Duration> expectedMap = Map.ofEntries(
+                Map.entry("inbox", Duration.of(150, MINUTES)),
+                Map.entry("break", Duration.of(40, MINUTES)),
+                Map.entry("ID-1872", Duration.of(50, MINUTES))
+        );
+
+        assertEquals(actual.keySet(), expectedMap.keySet());
+    }@Test
+    public void should_collect_all_time() {
+        List<Record> records = List.of(
+                new Record(1, LocalTime.of(10, 0, 0), "inbox", "начало"),
+                new Record(2, LocalTime.of(10, 30, 0), "break", "empty"),
+                new Record(3, LocalTime.of(11, 10, 0), "ID-1872", "делаю задачу"),
+                new Record(4, LocalTime.of(12, 0, 0), "inbox", "porn is good"),
+                new Record(5, LocalTime.of(14, 0, 0), "end", "empty")
+        );
+        Map<String, Duration> testMap = new ReportGenerator().generate(records);
+        Map<String, Duration> actual = new ReportGenerator().noBreaksGenerate(testMap);
+
+
+        Map<String, Duration> expectedMap = Map.ofEntries(
+                Map.entry("без перерывов", Duration.of(200, MINUTES)),
+                Map.entry("всего", Duration.of(240, MINUTES))
+        );
+        assertEquals(actual.keySet(), expectedMap.keySet());
+        var actualList = new ArrayList<>(actual.values());
+        var expectedList = new ArrayList<>(expectedMap.values());
+        actualList.sort(null);
+        expectedList.sort(null);
+
+        assertEquals(actualList, expectedList);
+        assertEquals(actual.entrySet(), expectedMap.entrySet());
     }
 }
